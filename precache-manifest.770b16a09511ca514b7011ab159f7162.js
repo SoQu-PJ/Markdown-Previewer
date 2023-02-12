529ed49153b8a0c6de3ad852ec025849accb2a81
SoQu-PJ <maniek999zano@gmail.com>
2023-02-12 14:37:53 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.770b16a09511ca514b7011ab159f7162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86180377f2b16435abff",
+    "url": "/Markdown-Previewer/static/css/main.b0e44c2d.chunk.css"
+  },
+  {
+    "revision": "86180377f2b16435abff",
+    "url": "/Markdown-Previewer/static/js/main.86180377.chunk.js"
+  },
+  {
+    "revision": "be3e9b6c5ece5d0fd99e",
+    "url": "/Markdown-Previewer/static/js/1.be3e9b6c.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/Markdown-Previewer/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "1ec78eae4653c0069270",
+    "url": "/Markdown-Previewer/static/js/runtime~main.1ec78eae.js"
+  },
+  {
+    "revision": "a657ecece9bc45d57c92861fdaa8367f",
+    "url": "/Markdown-Previewer/index.html"
+  }
+];
