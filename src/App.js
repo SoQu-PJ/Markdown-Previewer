74ddd8135d529d195a6f8f34a6c934aff7caa7a1
SoQu-PJ <maniek999zano@gmail.com>
2023-02-12 14:38:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { MarkdownPreviewer } from "./Components/MarkdownPreviewer";
+
+function App() {
+  return (
+    <MarkdownPreviewer />
+  );
+}
+
+export default App;
---
+++ src/Components/MarkdownPreviewer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const MarkdownPreviewer = () => {
+    return (
+    <h1>Hello World</h1>
+    );
+}
