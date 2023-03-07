e0190faff2017a4af707cd90986be7532b9711ff
SoQu-PJ <maniek999zano@gmail.com>
2023-03-07 20:08:29 +0100

    First css

--- src/Components/EditorSection.js
+++ src/Components/EditorSection.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "../App.css";
 
 export const EditorSection = props =>{
    
 
     return(
-        <>
+        <section className="editor-section">
+            <nav className="editor-nav"><i className="fa fa-free-code-camp"></i> Editor <i className="fa fa-arrows-alt"></i></nav>
             <textarea id="editor" value={props.textareaValue} onChange={e => props.setTextareaValue(e.target.value)}>
             
             </textarea>
-        </>
+        </section>
     );
 }
