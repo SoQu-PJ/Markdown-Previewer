e526d58d996abe95e01be7d098ffabe6623b92bb
SoQu-PJ <maniek999zano@gmail.com>
2023-03-08 21:16:36 +0100

    grid and style section

--- src/Components/PreviewerSection.js
+++ src/Components/PreviewerSection.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "../App.css";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm'
 
 export const PreviewerSection = props => {
     return (
         <section id="preview">
-            <ReactMarkdown children={props.textareaValue} remarkPlugins={[remarkGfm]} />
+            <nav className="previewer-nav"><i className="fa fa-free-code-camp"></i> Previewer <i className="fa fa-arrows-alt"></i></nav>
+            <div className="markdown-section">
+                <ReactMarkdown children={props.textareaValue} remarkPlugins={[remarkGfm]} />
+            </div>
         </section>
     );
 }
